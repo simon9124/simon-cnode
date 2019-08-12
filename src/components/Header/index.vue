7b2abf1a19a4e3de170fad7a238d0427a7679901
Simon9124 <435776313@qq.com>
2019-08-12 18:33:42 +0800

    header组件：1.组件布局结构；2.tabs布局结构

---
+++ src/components/Header/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container-header">
+    <div class="container-header-inner">
+
+      <!-- logo -->
+      <img src="http://static2.cnodejs.org/public/images/cnodejs_light.svg">
+
+      <!-- search -->
+      <input :value="searchValue" />
+
+      <!-- 头部导航栏 -->
+      <tab></tab>
+
+    </div>
+  </div>
+</template>
+
+<script>
+// components
+import Tab from '@/components/Header/tab';
+
+export default {
+  components: {
+    Tab
+  },
+  data () {
+    return {
+      searchValue: ''
+    };
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.container-header {
+  position: relative;
+  background: #444;
+  &-inner {
+    width: 90%;
+    margin: auto;
+    padding: 5px;
+    img {
+      width: 120px;
+      height: 28px;
+      padding: 10px 0;
+    }
+    input {
+      font-size: 13px;
+      width: 233px;
+      height: 26px;
+      background: url("../../../static/images/Search.png") no-repeat #888;
+      padding-left: 20px;
+      border-radius: 15px;
+    }
+  }
+}
+</style>
---
+++ src/components/Header/tab.vue
@@ -0,0 +1,42 @@
+<template>
+  <scroll-view class="tabs"
+               scroll-x>
+    <view v-for="(item,index) in navList"
+          :key="index"
+          class="tabs-item"
+          :data-current="index">{{item.title}}</view>
+    <!-- @tap="swichNav" -->
+  </scroll-view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      navList: [
+        { title: '首页', type: 'all' },
+        { title: '新手入门', type: 'good' },
+        { title: 'API', type: 'share' },
+        { title: '关于', type: 'ask' },
+        { title: '注册', type: 'job' },
+        { title: '登录', type: 'dev' }
+      ]
+    };
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.tabs {
+  width: 100%;
+  position: relative;
+  white-space: nowrap;
+  &-item {
+    font-size: 13px;
+    color: #fff;
+    padding: 10px 15px;
+    text-align: center;
+    display: inline-block;
+  }
+}
+</style>
