02fbc9c7b260967f9149d10e71c8f776badc9123
simon9124 <435776313@qq.com>
2020-07-14 13:41:25 +0800

    api更新

--- src/api/article/index.js
+++ src/api/article/index.js
@@ -1,12 +1,5 @@
 import fly from "@/utils/fly";
 
-// 获取页面内容 - api
-export function getHomeContent(tab, limit, page) {
-  return fly.request(`topics?tab=${tab}&limit=${limit}&page=${page}`, null, {
-    method: "get",
-  });
-}
-
 // 根据id获取文章内容
 export function getArticle(id) {
   return fly.request(`topic/${id}`, null, {
