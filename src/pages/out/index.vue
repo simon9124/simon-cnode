7b76fe5b90b3ff34ccd58bf7b6efecc26fe38361
simon9124 <435776313@qq.com>
2020-07-14 13:37:39 +0800

    页面模板：外链

---
+++ src/pages/out/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <web-view :src="href"></web-view>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      href: ""
+    };
+  },
+  onLoad () {
+    this.href = this.$root.$mp.query.href;
+  }
+};
+</script>
+
+<style>
+</style>
