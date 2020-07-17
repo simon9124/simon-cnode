5647c3827f7c34ea27c6a9c45b00d77f7851bc24
simon9124 <435776313@qq.com>
2020-07-17 18:11:54 +0800

    api更新

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,8 +1,16 @@
 import fly from "@/utils/fly";
+import flyGithub from "@/utils/fly.github";
 
 // 根据id获取文章内容
 export function getUser(name) {
   return fly.request(`user/${name}`, null, {
+    method: "get",
+  });
+}
+
+// 获取页面内容 - api
+export function getGithubPerson(name) {
+  return flyGithub.request(`users/${name}`, null, {
     method: "get",
   });
 }
