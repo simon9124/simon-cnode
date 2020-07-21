9abb0bb36a4227783bf017315fd561994d1f7d8f
simon9124 <435776313@qq.com>
2020-07-21 17:32:51 +0800

    api追加

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,16 +1,23 @@
 import fly from "@/utils/fly";
 import flyGithub from "@/utils/fly.github";
 
-// 根据id获取文章内容
+// 根据name获取用户信息
 export function getUser(name) {
   return fly.request(`user/${name}`, null, {
     method: "get",
   });
 }
 
-// 获取页面内容 - api
+// 根据githubUsername获取在github的用户信息
 export function getGithubPerson(name) {
   return flyGithub.request(`users/${name}`, null, {
+    method: "get",
+  });
+}
+
+// 根据name获取用户收藏
+export function getUserCollect(name) {
+  return fly.request(`topic_collect/${name}`, null, {
     method: "get",
   });
 }
