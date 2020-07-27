5726eda5770575dc65e88933ec3250380d227fc9
simon9124 <435776313@qq.com>
2020-07-27 16:19:38 +0800

    组件封装：home页跳转/滑动

---
+++ src/components/homeBack.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="breadcrumb-dashboard"
+        @click="goBackHome">主页</span>
+</template>
+
+<script>
+// function
+import Bus from "@/utils/bus";
+import appJson from "@/app.json";
+
+export default {
+  methods: {
+    goBackHome () {
+      var pages = getCurrentPages(); //当前页面
+      // console.log(pages[pages.length - 1]);
+      if (pages[pages.length - 1].route === appJson.homePage) {
+        // 在主页：切换标签回home -> 传递给兄弟组件
+        Bus.$emit("tabChange", 0);
+        // 登录页面切换回 "isLogin" 状态
+        Bus.$emit("isLogin", true);
+      } else {
+        // 不在主页：页面返回 - home
+        wx.reLaunch({
+          url: "/pages/index/main"
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
