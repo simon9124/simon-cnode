178fec1171dbc295824ee89c32441b0df870de4f
simon9124 <435776313@qq.com>
2020-08-05 17:51:40 +0800

    临时

--- src/mixins/backToTop.mixin.js
+++ src/mixins/backToTop.mixin.js
@@ -1,24 +1,22 @@
 let mixin = {
   data() {
     return {
-      // dataStack: [], // 解决mpvue相同组件数据不更新问题，建立栈堆
+      dataStack: [], // 解决mpvue相同组件数据不更新问题，建立栈堆
       scrollTop: null, // scroll-view 距离顶部的滚动高度
       backToTopDisplay: false, // "回到顶部" 是否显示
     };
   },
   onUnload() {
-    // Object.assign(this.$data, this.dataStack.pop()); // 恢复
+    Object.assign(this.$data, this.dataStack.pop()); // 恢复
   },
   onLoad() {
-    // this.dataStack.push({
-    // ...JSON.parse(JSON.stringify(this.$data))
-    // }); // 备份
+    this.dataStack.push({ ...JSON.parse(JSON.stringify(this.$data)) }); // 备份
   },
   methods: {
     // 监听scroll-view滚动
     scroll(e) {
       // console.log(e);
-      // this.scrollTop = e.mp.detail.scrollTop;
+      // this.scrollTopTemp = e.mp.detail.scrollTop;
       this.backToTopDisplay = e.mp.detail.scrollTop > 250;
     },
   },
